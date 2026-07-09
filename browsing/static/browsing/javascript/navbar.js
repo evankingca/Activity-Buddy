a7ef6cf2bd91d124dcc75a314e099e6f92f0d17b
evan <57847413+evankingca@users.noreply.github.com>
2026-07-09 00:44:12 -0400

    Make header and footer responsive

--- browsing/static/browsing/javascript/navbar.js
+++ browsing/static/browsing/javascript/navbar.js
@@ -1,13 +1,19 @@
+const navbar = document.getElementById("navbar")
+const bergieMenu = document.getElementById("bergieMenu")
+
 function navbar_scrollbar() {
     let prevScrollPos = window.pageYOffset;
     const header = document.getElementById("header")
     window.addEventListener("scroll", () => {
         let currentScrollPos = window.pageYOffset;
+        if (!navbar.classList.contains("collapsed")) {
+            navbar.classList.add("collapsed")
+        }
         if (prevScrollPos > currentScrollPos) {
             header.classList.add("top-0");
-            header.classList.remove("-top-24");
+            header.classList.remove("-top-28");
         } else {
-            header.classList.add("-top-24");
+            header.classList.add("-top-28");
             header.classList.remove("top-0");
         }
         prevScrollPos = currentScrollPos;
@@ -15,3 +21,11 @@ function navbar_scrollbar() {
 }
 
 window.addEventListener("load", navbar_scrollbar());
+
+bergieMenu.addEventListener("click", () => {
+    if (navbar.classList.contains("collapsed")) {
+        navbar.classList.remove("collapsed")
+    } else {
+        navbar.classList.add("collapsed")
+    }
+})
