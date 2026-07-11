720290fed06e44be5ae566902e82237d6428a0db
Rie Futaki <futa0004@algonquinlive.com>
2026-07-11 15:59:27 -0400

    Create login page form

--- browsing/views.py
+++ browsing/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, "browsing/index.html", context)
+
+def register(request):
+    context = {}
+    return render(request, "browsing/register.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "browsing/login.html", context)
