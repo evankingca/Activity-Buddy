5098f79ef6f5e39d5fc5dfd2b8f3d7f583db1686
Victoria Lane <victoria.lane.m@gmail.com>
2026-07-18 11:52:12 -0400

    Fix typo

--- gymgolf/.local_settings.example.py
+++ gymgolf/.local_settings.example.py
@@ -1,6 +1,6 @@
 # Copy this file to .local_settings.py and adjust values for your machine.
 # .local_settings.py is gitignored and will NOT be committed.
-# Windows us usually: NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+# Windows is usually: NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 
 NPM_BIN_PATH = "/usr/local/bin/npm"  # run `which npm` to find your path
