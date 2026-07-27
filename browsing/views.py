5c3f68229bf07d42cee0b1da95f187cf6b75d2d0
Victoria Lane <victoria.lane.m@gmail.com>
2026-07-27 07:41:39 -0400

    Add example context and conditional for selected message on Chat page

--- browsing/views.py
+++ browsing/views.py
@@ -52,7 +52,8 @@ def user_profile(request):
         "preferences": Preference.objects.filter( user_activity__user=request.user ),
         "connections": Connection.objects.filter( user_a=request.user ) | Connection.objects.filter( user_b=request.user ),
     }
-    return render(request, "browsing/user_profile.html", context)
+    return render(request, "browsing/chat.html", context)
+
 
 @login_required(login_url="/login/")
 def user(request):
@@ -69,10 +70,6 @@ def search(request):
 
 def about(request):
     return render(request, "browsing/about.html")
-
-def chat(request):
-    context = {}
-    return render(request, "browsing/chat.html", context)
 
 # -------------------------
 # Authentication Views
